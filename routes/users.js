5ceb0c3a48fdb75c15cb7bd819979134811aed38
Piscki Pratama <32217405+pisckipratama@users.noreply.github.com>
2023-04-06 22:38:51 +0700

    update for ci/cd trigger

--- routes/users.js
+++ routes/users.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET users listing. */
 router.get('/', function(req, res, next) {
-  res.send('respond with a resource from user route');
+  res.send('this is from user route');
 });
 
 module.exports = router;

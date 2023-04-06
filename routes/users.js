85b23c4b47688b25b0ccbaf6b35b95212b2703a7
Piscki Pratama <32217405+pisckipratama@users.noreply.github.com>
2023-04-06 22:43:41 +0700

    Update users.js

--- routes/users.js
+++ routes/users.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET users listing. */
 router.get('/', function(req, res, next) {
-  res.send('this is from user route');
+  res.send('this is from user route, and check keel will updating');
 });
 
 module.exports = router;
